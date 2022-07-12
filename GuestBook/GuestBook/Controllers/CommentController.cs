c186ae5b9e096ff4fff592cbca8c6198c9c828e0
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-07-12 13:16:09 +0200

    Implemented adding, updating and deleting

--- GuestBook/GuestBook/Controllers/CommentController.cs
+++ GuestBook/GuestBook/Controllers/CommentController.cs
@@ -44,96 +44,41 @@ namespace GuestBook.Controllers
             return View(comment);
         }
 
-        // GET: Comment/Create
-        public IActionResult Create()
+        // GET: Comment/AddOrEdit
+        public IActionResult AddOrEdit(int id = 0)
         {
-            return View();
+            if(id== 0)
+                return View(new Comment());
+            else
+                return View(_context.Comments.Find(id));
         }
 
-        // POST: Comment/Create
+        // POST: Comment/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Email,Description,CreatedDate")] Comment comment)
+        public async Task<IActionResult> AddOrEdit([Bind("Id,Name,Email,Description,CreatedDate")] Comment comment)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(comment);
+                if (comment.Id == 0)
+                {
+                    comment.CreatedDate = DateTime.Now;
+                    _context.Add(comment);
+                }
+                else
+                {
+                    _context.Update(comment);
+                    comment.CreatedDate = DateTime.Now;
+                }
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(comment);
         }
 
-        // GET: Comment/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Comments == null)
-            {
-                return NotFound();
-            }
-
-            var comment = await _context.Comments.FindAsync(id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-            return View(comment);
-        }
-
-        // POST: Comment/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Description,CreatedDate")] Comment comment)
-        {
-            if (id != comment.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(comment);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CommentExists(comment.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(comment);
-        }
-
-        // GET: Comment/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Comments == null)
-            {
-                return NotFound();
-            }
-
-            var comment = await _context.Comments
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-
-            return View(comment);
-        }
+        
 
         // POST: Comment/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -152,11 +97,6 @@ namespace GuestBook.Controllers
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CommentExists(int id)
-        {
-          return (_context.Comments?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
