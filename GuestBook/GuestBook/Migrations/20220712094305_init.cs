dfd1bcf2f5e67ce629a601f091cb6e6d38aa8373
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-07-12 11:44:41 +0200

    Created and configurated DbContext

---
+++ GuestBook/GuestBook/Migrations/20220712094305_init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GuestBook.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(30)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+        }
+    }
+}
---
+++ GuestBook/GuestBook/Models/CommentDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GuestBook.Models
+{
+    public class CommentDbContext: DbContext
+    {
+        public CommentDbContext(DbContextOptions<CommentDbContext> options):base(options)
+        {}
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
