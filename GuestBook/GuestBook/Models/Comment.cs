32df0cfb154a70d2175c65358b34e1580486cd95
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-07-12 11:27:48 +0200

    Created model

---
+++ GuestBook/GuestBook/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuestBook.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(30)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
